3aff2be0e016af4d87aa2d0e595a11b8bd666e81
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2024-02-11 18:06:41 +0100

    test: Improve basic tests

--- test/test.lisp
+++ test/test.lisp
@@ -4,90 +4,184 @@
 
 (test smoke
   "Initial smoke test for the incrementalist system."
-  (are-results '((inc::atom-wad ((0 0) (0 8))
-                  (:raw (inc:symbol-token :symbol ("FOO" "BAR")))
-                  (inc::word-wad ((0 0) (0 3)))
-                  (inc::word-wad ((0 5) (0 8)))))
-               (parse-result "foo::bar"))
-
-  (are-results '((inc::cons-wad ((0 0) (0 8)) ()
-                  (inc::cons-wad ((0 1) (0 8)) ()
-                   (inc::atom-wad ((0 2) (0 3))
-                    (:raw (inc:numeric-token :value 1)))
-                   (inc::atom-wad ((0 4) (0 5))
-                    (:raw (inc:numeric-token :value 2)))
-                   (inc::atom-wad ((0 6) (0 7))
-                    (:raw (inc:numeric-token :value 3))))))
-               (parse-result "'(1 2 3)"))
-
-  (are-results '((inc::cons-wad ((0 0) (0 13)) ()
-                  (inc::cons-wad ((0 1) (0 13)) ()
-                   (inc::labeled-object-definition-wad ((0 2) (0 6)) ()
-                    (inc::atom-wad ((0 5) (0 6))
-                     (:raw (inc:numeric-token :value 1))))
-                   (inc::labeled-object-reference-wad ((0 7) (0 10))
-                    (:raw (inc:numeric-token :value 1)))
-                   (inc::atom-wad ((0 11) (0 12))
-                    (:raw (inc:numeric-token :value 3))))))
-               (parse-result "'(#1=1 #1# 3)")))
-
-(defun expected-consing-dot (location)
-  `(inc::atom-wad ,location (:raw (symbol)))) ; TODO make this a token?
-
-(defun expected-symbol-wad (location symbol-name
-                            &key (package-name "INCREMENTALIST.TEST.TEST-PACKAGE")
-                                 (token-class  'inc:non-existing-symbol-token)
-                                 (words        `((inc::word-wad ,location))))
-  `(inc::atom-wad ,location
-    (:raw (,token-class :symbol (,package-name ,symbol-name)))
-    ,@words))
+  (analysis-cases ()
+    ("#\\Return"
+     '((inc::atom-wad ((0 0) (0 8))
+        ; TODO (:raw (inc:value-token :value #\Return))
+        )))
+    ("\"foo\""
+     '((inc::atom-wad ((0 0) (0 5))
+        () ; TODO (:raw (inc:value-token :value #\Return))
+        (inc::word-wad ((0 1) (0 4))))))
+    ("foo::bar"
+     '((inc::atom-wad ((0 0) (0 8))
+        (:raw (inc:symbol-token :symbol ("FOO" "BAR")))
+        (inc::word-wad ((0 0) (0 3)))
+        (inc::word-wad ((0 5) (0 8))))))
+    ("'(1 2 3)"
+     '((inc::cons-wad ((0 0) (0 8)) ()
+        (inc::cons-wad ((0 1) (0 8)) ()
+         (inc::atom-wad ((0 2) (0 3))
+          (:raw (inc:numeric-token :value 1)))
+         (inc::atom-wad ((0 4) (0 5))
+          (:raw (inc:numeric-token :value 2)))
+         (inc::atom-wad ((0 6) (0 7))
+          (:raw (inc:numeric-token :value 3)))))))
+    ("'(#1=1 #1# 3)"
+     '((inc::cons-wad ((0 0) (0 13)) ()
+        (inc::cons-wad ((0 1) (0 13)) ()
+         (inc::labeled-object-definition-wad ((0 2) (0 6)) ()
+          (inc::atom-wad ((0 5) (0 6))
+           (:raw (inc:numeric-token :value 1))))
+         (inc::labeled-object-reference-wad ((0 7) (0 10))
+          (:raw (inc:numeric-token :value 1)))
+         (inc::atom-wad ((0 11) (0 12))
+          (:raw (inc:numeric-token :value 3)))))))
+    #+TODO ("#+a b"
+     `((inc::sharpsign-plus-wad ((0 0) (0 5))
+       (:raw (inc:non-existing-symbol-token :symbol ("INCREMENTALIST.TEST.TEST-PACKAGE" "B")))
+       ,(expected-symbol-wad '((0 2) (0 3)) "A" :token-class  'inc:existing-symbol-token
+                                                :package-name "KEYWORD")
+       ,(expected-symbol-wad '((0 4) (0 5)) "B"))))
+    ("#-a b"
+     `((inc::atom-wad ((0 0) (0 5)) ; TODO should be sharpsign-minus-wad
+        (:raw (inc:non-existing-symbol-token :symbol ("INCREMENTALIST.TEST.TEST-PACKAGE" "B")))
+        ,(expected-symbol-wad '((0 2) (0 3)) "A" :token-class  'inc:existing-symbol-token
+                                                 :package-name "KEYWORD")
+        ,(expected-symbol-wad '((0 4) (0 5)) "B"))))))
 
 (test incomplete
-  (are-results
-   `((inc::cons-wad ((0 0) (0 19)) ()
-                    (inc::cons-wad ((0 1) (0 8)) ()
-                                   ,(expected-symbol-wad '((0 2) (0 3)) "A")
-                                   ,(expected-consing-dot '((0 4) (0 5)))
-                                   ,(expected-symbol-wad '((0 6) (0 7)) "B"))
-                    ,(expected-consing-dot '((0 9) (0 10)))
-                    (inc::cons-wad ((0 11) (0 18)) ()
-                                   ,(expected-symbol-wad '((0 12) (0 13)) "C")
-                                   ,(expected-consing-dot '((0 14) (0 15)))
-                                   ,(expected-symbol-wad '((0 16) (0 17)) "D"))))
-   (parse-result "((a . b) . (c . d))"))
+  "Tests for incomplete expressions."
+  (analysis-cases ()
+    ("(a #||#)"
+     `((inc::cons-wad ((0 0) (0 8)) ()
+        ,(expected-symbol-wad '((0 1) (0 2)) "A")
+        (inc:block-comment-wad ((0 3) (0 7))))))
+    ("((a . b) . ; foo"
+     `((inc::cons-wad ((0 0) (0 16))
+        (:errors ((((0 16) (0 16)) eclector.reader::unterminated-list)
+                  (((0 16) (0 16)) eclector.reader::end-of-input-after-consing-dot)))
+        (inc::cons-wad ((0 1) (0 8)) ()
+         ,(expected-symbol-wad '((0 2) (0 3)) "A")
+         ,(expected-consing-dot '((0 4) (0 5)))
+         ,(expected-symbol-wad '((0 6) (0 7)) "B"))
+        ,(expected-consing-dot '((0 9) (0 10)))
+        (inc::semicolon-comment-wad ((0 11) (0 16)) () ; TODO helper function
+         (inc::word-wad ((0 13) (0 16)))))))
 
-  (are-results
-   `((inc::cons-wad ((0 0) (0 15))
-                    (:errors ((((0 15) (0 15)) eclector.reader::unterminated-list)))
-                    (inc::cons-wad ((0 1) (0 8)) ()
-                                   ,(expected-symbol-wad '((0 2) (0 3)) "A")
-                                   ,(expected-consing-dot '((0 4) (0 5)))
-                                   ,(expected-symbol-wad '((0 6) (0 7)) "B"))
-                    ,(expected-consing-dot '((0 9) (0 10)))
-                    (inc::cons-wad ((0 11) (0 15))
-                                   (:errors ((((0 15) (0 15)) eclector.reader::unterminated-list)
-                                             (((0 15) (0 15)) eclector.reader::end-of-input-after-consing-dot)))
-                                   ,(expected-symbol-wad '((0 12) (0 13)) "C")
-                                   ,(expected-consing-dot '((0 14) (0 15))))))
-   (parse-result "((a . b) . (c ."))
+    ("((a . b) . (c ."
+     `((inc::cons-wad ((0 0) (0 15))
+        (:errors ((((0 15) (0 15)) eclector.reader::unterminated-list)))
+        (inc::cons-wad ((0 1) (0 8)) ()
+         ,(expected-symbol-wad '((0 2) (0 3)) "A")
+         ,(expected-consing-dot '((0 4) (0 5)))
+         ,(expected-symbol-wad '((0 6) (0 7)) "B"))
+        ,(expected-consing-dot '((0 9) (0 10)))
+        (inc::cons-wad ((0 11) (0 15))
+         (:errors ((((0 15) (0 15)) eclector.reader::unterminated-list)
+                   (((0 15) (0 15)) eclector.reader::end-of-input-after-consing-dot)))
+         ,(expected-symbol-wad '((0 12) (0 13)) "C")
+         ,(expected-consing-dot '((0 14) (0 15)))))))
 
-  (are-results `((inc::cons-wad ((0 0) (0 16))
-                 (:errors ((((0 16) (0 16)) eclector.reader::unterminated-list)
-                           (((0 16) (0 16)) eclector.reader::end-of-input-after-consing-dot)))
-                 (inc::cons-wad ((0 1) (0 8)) ()
-                  ,(expected-symbol-wad '((0 2) (0 3)) "A")
-                  ,(expected-consing-dot '((0 4) (0 5)))
-                  ,(expected-symbol-wad '((0 6) (0 7)) "B"))
-                 ,(expected-consing-dot '((0 9) (0 10)))
-                 (inc::semicolon-comment-wad ((0 11) (0 16)) () ; TODO helper function
-                  (inc::word-wad ((0 13) (0 16))))))
-               (parse-result "((a . b) . ; foo")))
+    ("((a . b) . (c . d))"
+     `((inc::cons-wad ((0 0) (0 19)) ()
+        (inc::cons-wad ((0 1) (0 8)) ()
+         ,(expected-symbol-wad '((0 2) (0 3)) "A")
+         ,(expected-consing-dot '((0 4) (0 5)))
+         ,(expected-symbol-wad '((0 6) (0 7)) "B"))
+        ,(expected-consing-dot '((0 9) (0 10)))
+        (inc::cons-wad ((0 11) (0 18)) ()
+         ,(expected-symbol-wad '((0 12) (0 13)) "C")
+         ,(expected-consing-dot '((0 14) (0 15)))
+         ,(expected-symbol-wad '((0 16) (0 17)) "D")))))
+
+    ("((a . b) . (c . d) #||#)"
+     `((inc::cons-wad ((0 0) (0 24)) ()
+        (inc::cons-wad ((0 1) (0 8)) ()
+         ,(expected-symbol-wad '((0 2) (0 3)) "A")
+         ,(expected-consing-dot '((0 4) (0 5)))
+         ,(expected-symbol-wad '((0 6) (0 7)) "B"))
+        ,(expected-consing-dot '((0 9) (0 10)))
+        (inc::cons-wad ((0 11) (0 18)) ()
+         ,(expected-symbol-wad '((0 12) (0 13)) "C")
+         ,(expected-consing-dot '((0 14) (0 15)))
+         ,(expected-symbol-wad '((0 16) (0 17)) "D"))
+        (inc::block-comment-wad ((0 19) (0 23))))))
+
+    ("((a . b) c . d #||#)"
+     `((inc::cons-wad ((0 0) (0 20)) ()
+       (inc::cons-wad ((0 1) (0 8)) ()
+        ,(expected-symbol-wad '((0 2) (0 3)) "A")
+        ,(expected-consing-dot '((0 4) (0 5)))
+        ,(expected-symbol-wad '((0 6) (0 7)) "B"))
+       ,(expected-symbol-wad '((0 9) (0 10)) "C")
+       ,(expected-consing-dot '((0 11) (0 12)))
+       ,(expected-symbol-wad '((0 13) (0 14)) "D")
+       (inc::block-comment-wad ((0 15) (0 19))))))
+
+    ("((a . b) #||# #||# . (c . d) #||#)"
+     `((inc::cons-wad ((0 0) (0 34)) ()
+       (inc::cons-wad ((0 1) (0 8)) ()
+        ,(expected-symbol-wad '((0 2) (0 3)) "A")
+        ,(expected-consing-dot '((0 4) (0 5)))
+        ,(expected-symbol-wad '((0 6) (0 7)) "B"))
+       (inc::block-comment-wad ((0 9) (0 13)))
+       (inc::block-comment-wad ((0 14) (0 18)))
+       ,(expected-consing-dot '((0 19) (0 20)))
+       (inc::cons-wad ((0 21) (0 28)) ()
+        ,(expected-symbol-wad '((0 22) (0 23)) "C")
+        ,(expected-consing-dot '((0 24) (0 25)))
+        ,(expected-symbol-wad '((0 26) (0 27)) "D"))
+       (inc::block-comment-wad ((0 29) (0 33))))))
+
+    ("`(a . (b ,c))"
+     `((inc::cons-wad ((0 0) (0 13)) ()
+        (inc::cons-wad ((0 1) (0 13)) ()
+         ,(expected-symbol-wad '((0 2) (0 3)) "A")
+         ,(expected-consing-dot '((0 4) (0 5)))
+         (inc::cons-wad ((0 6) (0 12)) ()
+          ,(expected-symbol-wad '((0 7) (0 8)) "B")
+         (inc::cons-wad ((0 9) (0 11)) ()
+          ,(expected-symbol-wad '((0 10) (0 11)) "C")))))))))
 
 (test errors
   "Tests for errors."
-  (are-results '((inc::atom-wad ((0 0) (0 16))
-                  (:errors ((((0 2) (0 16)) eclector.reader:unknown-character-name)))))
-               (parse-result "#\\does-not-exist"))
-  (are-results '((inc::atom-wad ((0 0) (0 7))
-                  (:errors ((((0 4) (0 5)) eclector.reader:digit-expected)))))
-               (parse-result "#b00200")))
+  (analysis-cases ()
+    ("#\\does-not-exist"
+     '((inc::atom-wad ((0 0) (0 16))
+        (:errors ((((0 2) (0 16)) eclector.reader:unknown-character-name))))))
+    ("#b00200"
+     '((inc::atom-wad ((0 0) (0 7))
+        (:errors ((((0 4) (0 5)) eclector.reader:digit-expected))))))))
+
+(test todo
+  (finishes (parse-result "(
+; a
+; b
+(1 . 2)
+; c
+; d
+3"))
+  (finishes (parse-result "((a . b) . (c ."))
+
+  (finishes (parse-result "(
+; a
+; b
+(1 . 2)
+; c
+.
+; d
+3"))
+
+
+  (finishes (parse-result "(
+; a
+(1 . 2)
+#||#
+.
+#||#
+3"))
+
+  (finishes (parse-result ",`bar"))
+
+  (finishes (parse-result "`foo, `bar")))
